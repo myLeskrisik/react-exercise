cb700c455fba8395abdf8c9e8d5bbf62155f0efd
myLeskrisik <smyles0763@icloud.com>
2022-06-24 14:12:13 -0700

    added more style

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -1,7 +1,26 @@
 import ToDoList from "./Components/ToDoList";
 
+const style = {
+  html: {
+    backgroundColor: "#573d9d",
+    height: "1000px",
+    margin: "0",
+  },
+};
+
 function App() {
-  return <ToDoList />;
+  return (
+    <>
+      <html style={style.html}>
+        <head>
+          <meta charSet="UTF-8" />
+        </head>
+        <body>
+          <ToDoList />
+        </body>
+      </html>
+    </>
+  );
 }
 
 export default App;
--- react-exercise/src/Components/ToDoItem.js
+++ react-exercise/src/Components/ToDoItem.js
@@ -15,13 +15,21 @@ const style = {
   liText: {
     fontSize: "40px",
   },
+  li: {
+    listStyle: "none",
+    backgroundColor: "#fd9300",
+    width: "25%",
+    padding: "10px 25px 10px 25px",
+    borderRadius: "20px",
+    margin: "20px 0px 20px 0px",
+  },
 };
 
 const ToDoItem = ({ content, details }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <li onClick={() => setOpen(true)} key={details}>
+      <li onClick={() => setOpen(true)} style={style.li} key={details}>
         <p style={style.liText}>{content}</p>
       </li>
       <Modal
